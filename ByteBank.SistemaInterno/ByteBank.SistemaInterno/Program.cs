fd927b14105002e2dc4e6f0de24c8b0bfefa7438
Marzeu <marzahell@hotmail.com>
2021-03-25 10:02:44 -0300

    dll

---
+++ ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(456, 455789);
+        }
+    }
+}
